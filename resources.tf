b0b80b7b43548178e3152e2973491d38aa9ebd73
unknown <thulsidk1@gmail.com>
2022-03-29 23:06:37 +0530

    29-03-2022

--- resources.tf
+++ resources.tf
@@ -26,7 +26,7 @@ resource "aws_eip" "ip" {
 }
 resource "aws_nat_gateway" "natgw1" {
  allocation_id="${aws_eip.ip.id}"
-  subnet_id = "${aws_subnet.subnet_private1.id}"
+  subnet_id = "${aws_subnet.subnet_public.id}"
   tags = {
     "Environment" = "${var.environment_tag}"
   }
@@ -112,7 +112,176 @@ resource "aws_security_group" "sg_22" {
   }
 }
 
+resource "aws_security_group" "sg_221" {
+  name = "sg_221"
+  vpc_id = "${aws_vpc.vpc.id}"
 
+  # SSH access from the VPC
+  ingress {
+      from_port   = 22
+      to_port     = 22
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+	egress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    "Environment" = "${var.environment_tag}"
+  }
+}
+
+resource "aws_network_acl" "My_VPC_Security_ACL" {
+  vpc_id = "${aws_vpc.vpc.id}"
+  subnet_ids = ["${aws_subnet.subnet_private1.id}"]
+# allow ingress port 1024 to 65535
+  ingress {
+    protocol   = "tcp"
+    rule_no    = 6
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 1024
+    to_port    = 65535
+  }
+
+  # allow ingress port 80
+  ingress {
+    protocol   = "tcp"
+    rule_no    = 6
+    action     = "allow"
+    cidr_block = "${var.cidr_subnet}"
+    from_port  = 80
+    to_port    = 80
+  }
+
+
+
+  # allow egress port 80
+  egress {
+    protocol   = "tcp"
+    rule_no    = 6
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 80
+    to_port    = 80
+  }
+
+  # allow egress port 80
+  egress {
+    protocol   = "tcp"
+    rule_no    = 6
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 443
+    to_port    = 443
+  }
+
+  # allow egress ephemeral ports
+  egress {
+    protocol   = "tcp"
+    rule_no    = 6
+    action     = "allow"
+    cidr_block = "${var.cidr_subnet}"
+    from_port  = 80
+    to_port    = 80
+  }
+
+  # allow egress ephemeral ports
+    egress {
+      protocol   = "tcp"
+      rule_no    = 6
+      action     = "allow"
+      cidr_block = "${var.cidr_subnet}"
+      from_port  = 443
+      to_port    = 443
+  }
+tags = {
+    Name = "My VPC ACL"
+}
+}
+
+resource "aws_network_acl" "My_VPC_Security_ACL1" {
+  vpc_id = "${aws_vpc.vpc.id}"
+  subnet_ids = ["${aws_subnet.subnet_public.id}"]
+# allow ingress port 1024 to 65535
+  ingress {
+    protocol   = "tcp"
+    rule_no    = 6
+    action     = "allow"
+    cidr_block = "${var.cidr_subnet1}"
+    from_port  = 443
+    to_port    = 443
+  }
+
+  # allow ingress port 80
+  ingress {
+    protocol   = "tcp"
+    rule_no    = 6
+    action     = "allow"
+    cidr_block = "${var.cidr_subnet1}"
+    from_port  = 80
+    to_port    = 80
+  }
+	# allow ingress port 80
+  ingress {
+    protocol   = "tcp"
+    rule_no    = 6
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 1024
+    to_port    = 65535
+  }
+
+
+
+
+  # allow egress port 80
+  egress {
+    protocol   = "tcp"
+    rule_no    = 6
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 443
+    to_port    = 443
+  }
+
+  # allow egress port 80
+  egress {
+    protocol   = "tcp"
+    rule_no    = 6
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 80
+    to_port    = 80
+  }
+
+  # allow egress ephemeral ports
+  egress {
+    protocol   = "tcp"
+    rule_no    = 6
+    action     = "allow"
+    cidr_block = "${var.cidr_subnet1}"
+    from_port  = 1024
+    to_port    = 65535
+  }
+
+
+tags = {
+    Name = "My VPC ACL1"
+}
+}
 
 resource "tls_private_key" "example" {
   algorithm = "RSA"
@@ -129,6 +298,17 @@ resource "aws_instance" "testInstance" {
   subnet_id = "${aws_subnet.subnet_public.id}"
 	key_name = "lakshminarsimha"
   vpc_security_group_ids = [aws_security_group.sg_22.id]
+  tags = {
+		"Environment" = "${var.environment_tag}"
+	}
+}
+
+resource "aws_instance" "testInstance1" {
+  ami           = "${var.instance_ami}"
+  instance_type = "${var.instance_type}"
+  subnet_id = "${aws_subnet.subnet_private1.id}"
+	key_name = "lakshminarsimha"
+  vpc_security_group_ids = [aws_security_group.sg_221.id]
   tags = {
 		"Environment" = "${var.environment_tag}"
 	}
