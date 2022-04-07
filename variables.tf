53a1f71c5fa766d84e6cf1134f6ded4aef40c678
unknown <thulsidk1@gmail.com>
2022-04-07 20:26:41 +0530

    associate public IP to instance

--- variables.tf
+++ variables.tf
@@ -12,6 +12,11 @@ variable "cidr_subnet" {
   default     = "10.1.0.0/24"
 }
 
+variable "cidr_subnet1" {
+  description = "CIDR block for the subnet1"
+  default     = "10.1.1.0/28"
+}
+
 variable "cidr_subnet2" {
   description = "CIDR block for the subnet1"
   default     = "10.2.1.0/1"
@@ -36,8 +41,8 @@ data "aws_ami" "my_awslinux" {
     values = ["*-2.0.20180810-*"]
   }
   filter {
-      name   = "virtualization-type"
-      values = ["hvm"]
+    name   = "virtualization-type"
+    values = ["hvm"]
   }
 }
 
